22cb259217bf848fef3017f862e01fa29f6cb3d5
Edward O'Connor <hober0@gmail.com>
2010-11-30 13:39:06 +0100

    update

--- edom.el
+++ edom.el
@@ -5,6 +5,7 @@
 ;; Author: Edward O'Connor <hober0@gmail.com>
 ;; Keywords: xml
 ;; URL: http://edward.oconnor.cx/elisp/edom.el
+;; Version: 1.0.0
 
 ;; This file is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the
